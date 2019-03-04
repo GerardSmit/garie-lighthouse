6c5eb4d6bc1b42cc475e86ef09d6c1f821546d5b
Gerard Smit <g.smit@expedient.nl>
2019-03-04 18:36:32 +0100

    Changed file name for Windows

--- src/utils/save-report/index.js
+++ src/utils/save-report/index.js
@@ -7,7 +7,7 @@ module.exports = async (url, data) => {
     try {
         const report = await generateReport(url, data);
         const date = new Date();
-        return fs.outputFile(path.join(__dirname, '../../../reports', url.replace(/(^\w+:|^)\/\//, ''), `${new Date().toISOString()}.html`), report);
+        return fs.outputFile(path.join(__dirname, '../../../reports', url.replace(/(^\w+:|^)\/\//, ''), `${new Date().toISOString().substring(0, 19).replace(/[:-]/g, '').replace('T', '-')}.html`), report);
     } catch (err) {
         logger.error(`Failed to generate report for ${url}`, err);
         return Promise.reject('Failed to generate report');
